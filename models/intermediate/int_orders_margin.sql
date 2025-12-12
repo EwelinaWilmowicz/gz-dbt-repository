828e9743ade5166db5c3cd0084ae5b81ad4db490
Ewelina Wilmowicz <evvelliine@gmail.com>
2025-12-12 20:28:53 +0000

    int_orders_margin added

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,20 @@
+with sales_margin as (
+
+    select {{ ref ('int_sales_margin')}}
+
+), 
+
+orders_margin as (
+
+    select
+    orders_id,
+    min(date_date) as date_date,
+    sum(revenue) as revenue,
+    sum(quantity) as quantity,
+    sum(purchase_cost) as purchase_cost,
+    sum(margin) as margin
+
+    from sales_margin
+    GROUP BY orders_id
+
+)

0af6eecee7d8c047a76ee0e1e72efe7c4dbc739b
Ewelina Wilmowicz <evvelliine@gmail.com>
2025-12-15 14:13:36 +0000

    used dbt_utils.union_relations for int_campaigns

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,64 +1,23 @@
-with adwords as (
-    select
-        date_date,
-        paid_source,
-        cast(campaign_key as string) as campaign_id,
-        campaign_name,
-        'adwords' as source,
-        cast(ads_cost as float64) as ads_cost,
-        cast(impression as int64) as impression,
-        cast(click as int64) as click
-    from {{ ref('stg_raw__adwords') }}
-),
+with unioned as (
 
-bing as (
-    select
-        date_date,
-        paid_source,
-        cast(campaign_key as string) as campaign_id,
-        campaign_name,
-        'bing' as source,
-        cast(ads_cost as float64) as ads_cost,
-        cast(impression as int64) as impression,
-        cast(click as int64) as click
-    from {{ ref('stg_raw__bing') }}
-),
+  {{ dbt_utils.union_relations(
+      relations=[
+        ref('stg_raw__adwords'),
+        ref('stg_raw__bing'),
+        ref('stg_raw__criteo'),
+        ref('stg_raw__facebook')
+      ]
+  ) }}
 
-criteo as (
-    select
-        date_date,
-        paid_source,
-        cast(campaign_key as string) as campaign_id,
-        campaign_name,
-        'criteo' as source,
-        cast(ads_cost as float64) as ads_cost,
-        cast(impression as int64) as impression,
-        cast(click as int64) as click
-    from {{ ref('stg_raw__criteo') }}
-),
-
-facebook as (
-    select
-        date_date,
-        paid_source,
-        cast(campaign_key as string) as campaign_id,
-        campaign_name,
-        'facebook' as source,
-        cast(ads_cost as float64) as ads_cost,
-        cast(impression as int64) as impression,
-        cast(click as int64) as click
-    from {{ ref('stg_raw__facebook') }}
-),
-
-unioned as (
-    select * from adwords
-    union all
-    select * from bing
-    union all
-    select * from criteo
-    union all
-    select * from facebook
 )
 
-select *
+select
+  date_date,
+  paid_source,
+  cast(campaign_key as string) as campaign_id,
+  campaign_name,
+  lower(regexp_extract(_dbt_source_relation, r"stg_raw__([a-zA-Z0-9_]+)")) as source,
+  cast(ads_cost as float64) as ads_cost,
+  cast(impression as int64) as impression,
+  cast(click as int64) as click
 from unioned
